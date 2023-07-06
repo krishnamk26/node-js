bbcfe6752bc29d6698e36cdd937b5c58ca6af3bc
krishnamk26 <krissurya1@gmail.com>
2023-07-07 01:36:56 +0530

    remove node

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/',(req,res)=>{
-    res.send('<h1> Welcome to Express </h1>')
+    res.send('<h1> Welcome to Express App </h1>')
 })
 
 router.post('/',(req,res)=>{
