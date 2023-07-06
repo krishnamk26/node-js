73c79174bbfe59f475865dbdee272196d5a22044
krishnamk26 <krissurya1@gmail.com>
2023-07-07 01:54:55 +0530

    port update

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const indexRouter = require('./routes/index')
 const dashboardRouter = require('./routes/dashboard')
 const userRouter = require('./routes/users')
+const PORT = process.env.port || 8000
 app.use(express.json())
 app.use('/',indexRouter)
 app.use('/dashboard',dashboardRouter)
